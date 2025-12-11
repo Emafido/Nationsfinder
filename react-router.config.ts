0a87770096c7cdd1c903d40ce948839ef030fd2f
Emafido <emmanuelemafido@gmail.com>
2025-12-11 18:23:01 +0100

    one more time?

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  // Server-side render by default
   ssr: true,
+  // For Vercel deployment
+  serverBuildPath: "build/server/index.js",
 } satisfies Config;
